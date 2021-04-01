46d6416f731a93135e3cf1ede0d35e8e42994c63
kasra <kasramagers1377@gmail.com>
2021-04-02 02:33:07 +0430

    add carry-look-ahead

---
+++ hw1/hw1-carry-look-ahead.vhdl
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity carry_look_ahead_adder is 
+port(
+   input1,input2,carryIn : in bit;
+   sum,generate,propagate : out bit);
+end entity;
+
+architecture struct of carry_look_ahead_adder is
+
+begin
+    sum <= (input1 xor input2) xor carryIn;
+    propagate <= input1 xor input2;
+    generate <= input1 and input2;
+end architecture;
