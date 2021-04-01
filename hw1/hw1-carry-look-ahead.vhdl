b84e7e93805421a127637a872634a5f18ba114ec
kasra <kasramagers1377@gmail.com>
2021-04-02 03:30:48 +0430

    add q4 for hw1

--- hw1/hw1-carry-look-ahead.vhdl
+++ hw1/hw1-carry-look-ahead.vhdl
@@ -5,7 +5,7 @@ use ieee.std_logic_1164.all;
 entity carry_look_ahead_adder is 
 port(
    input1,input2,carryIn : in bit;
-   sum,generate,propagate : out bit);
+   sum,gen,propagate : out bit);
 end entity;
 
 architecture struct of carry_look_ahead_adder is
@@ -13,5 +13,5 @@ architecture struct of carry_look_ahead_adder is
 begin
     sum <= (input1 xor input2) xor carryIn;
     propagate <= input1 xor input2;
-    generate <= input1 and input2;
+    gen <= input1 and input2;
 end architecture;
