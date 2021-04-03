f8c2424db89eb6b82089824c1bc49d1306166196
alireza13781999 <alirezanemati1999@gmail.com>
2021-04-03 22:16:59 +0430

    last changes

--- hw1/q2/BCD2seg.vhd
+++ hw1/q2/BCD2seg.vhd
@@ -9,21 +9,41 @@ entity BCD2seg is
 	
 architecture decoder_arch of BCD2seg is 
 	begin
-		with w select
-			seg <= "1111110" when "0000",
-					 "0110000" when "0001",
-					 "1101101" when "0010",
-					 "1111001" when "0011",
-					 "0110011" when "0100",
-					 "1011011" when "0101",
-					 "1011111" when "0110",
-					 "1110000" when "0111",
-					 "1111111" when "1000",
-					 "1111011" when "1001",
-					 "1111101" when "1010",
-					 "0011111" when "1011",
-					 "1001110" when "1100",
-					 "0111101" when "1101",
-					 "1001111" when "1110",
-					 "1000111" when others;
+		process(w)
+		begin
+		case w is
+			when "0000" => 
+				seg <= "1111110";
+			when "0001" => 
+				seg <= "0110000";
+			when "0010" => 
+				seg <= "1101101";
+			when "0011" => 
+				seg <= "1111001";
+			when "0100" => 
+				seg <= "0110011";
+			when "0101" => 
+				seg <= "1011011";
+			when "0110" => 
+				seg <= "1011111";
+			when "0111" => 
+				seg <= "1110000";
+			when "1000" => 
+				seg <= "1111111";
+			when "1001" => 
+				seg <= "1111011";
+			when "1010" => 
+				seg <= "1111101";
+			when "1011" => 
+				seg <= "0011111";
+			when "1100" => 
+				seg <= "1001110";
+			when "1101" => 
+				seg <= "0111101";
+			when "1110" => 
+				seg <= "1001111";
+			when others => 
+				seg <= "1000111";
+		end case;
+		end process;
 	end decoder_arch;
